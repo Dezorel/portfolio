63c014234a483db5491d46de1cfcf78bd5cd3312
Leonid <scorpion23997@gmail.com>
2021-03-16 18:21:50 +0200

    conect curentAtrcile to API

--- projects/naukin/blog/currentArticle/curArticleScrtipt.js
+++ projects/naukin/blog/currentArticle/curArticleScrtipt.js
@@ -2,10 +2,11 @@ let app = new Vue({
     el: '#app',
     data(){
         return{
-            postName: ''
+            postName: '',
+            posts: null,
         }
     },
-    created(){
+    async created(){
         let url = window.location.hash
         if(url){
             this.postName = url.substr(1,url.length)
@@ -13,5 +14,9 @@ let app = new Vue({
             let result = arrayOfStrings.join(' ').trim()
             console.log(result)
         }
+        let link = 'https://jsonplaceholder.typicode.com/posts'
+        let res = await fetch(link)
+        this.posts = await res.json()        //получаю данные в json
+        console.log(this.posts)
     }
 })
