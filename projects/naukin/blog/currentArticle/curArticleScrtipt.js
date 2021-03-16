b7a01bd9fab46a711c94f49cc42fe1b38678a4ce
Leonid <scorpion23997@gmail.com>
2021-03-16 19:18:16 +0200

    update

--- projects/naukin/blog/currentArticle/curArticleScrtipt.js
+++ projects/naukin/blog/currentArticle/curArticleScrtipt.js
@@ -8,12 +8,12 @@ let app = new Vue({
     },
     async created(){
         let url = window.location.hash
-        if(url){
-            this.postName = url.substr(1,url.length)
-            let arrayOfStrings = this.postName.split('-')
-            let result = arrayOfStrings.join(' ').trim()
-            console.log(result)
-        }
+        this.postName = url.substr(1,url.length)
+        let arrayOfStrings = this.postName.split('-')       //это не нужно
+        let result = arrayOfStrings.join(' ').trim()        //это не нужно
+        console.log(result)
+
+
         let link = 'https://jsonplaceholder.typicode.com/posts'
         let res = await fetch(link)
         this.posts = await res.json()        //получаю данные в json
